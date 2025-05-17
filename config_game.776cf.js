c7b56d8fc8bf4c12176e9e22926cf2283227a5d3
minhhien22 <ndmhien220796@gmail.com>
2025-05-17 22:28:35 +0700

    aaa

--- config_game.776cf.js
+++ config_game.776cf.js
@@ -1,6 +1,6 @@
 window.gameConfig = {
 	"api_login_capcha":"https://testsieuno.club/",
-	"api_domain":"rongvang68.online",
+	"api_domain":"rongvang68.com",
 	"link_android":"https://nangchoichang.online/download/NgonClub.apk",
 	"link_ios":"https://apps.apple.com/vn/app/ngon-gastronomic-journey/id6503113331"
 }
